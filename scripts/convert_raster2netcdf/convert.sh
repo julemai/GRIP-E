238dff0e6324e7fb86a622410543fc47d5c31907
Juliane Mai <juliane.mai@uwaterloo.ca>
2018-11-28 15:11:43 -0500

    adding licence to file

--- scripts/convert_raster2netcdf/convert.sh
+++ scripts/convert_raster2netcdf/convert.sh
@@ -1,6 +1,25 @@
-#!/bin/bash
+#! /bin/bash
+
+# Copyright 2018 Juliane Mai - juliane.mai(at)uwaterloo.ca
 #
-# Produces plots for presentation.
+# License
+# This file is part of Juliane Mai's personal code library.
+#
+# Juliane Mai's personal code library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Juliane Mai's personal code library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with Juliane Mai's personal code library.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Converts ArcGIS raster format files to NetCDF.
 #
 set -e
 #
