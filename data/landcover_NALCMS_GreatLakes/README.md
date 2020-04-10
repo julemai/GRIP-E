88ac6bace0331d4a0b0a709adb17f90802a83349
Juliane Mai <juliane.mai@uwaterloo.ca>
2020-04-10 14:01:45 -0400

    Resolve issue with lat/lon coordinates used for aggregation of landcover (v1.1) and increasing the extracted landcover data to cover both RDRS and WFDEI-GEM-CaPA

--- data/landcover_NALCMS_GreatLakes/README.md
+++ data/landcover_NALCMS_GreatLakes/README.md
@@ -1,11 +1,11 @@
 ## NALCMS, 19 land cover classes for North America (30m, Landsat, 2010 from Mexico and Canada, 2011 for U.S.)
 
 External download from:<br>
-http://www.civil.uwaterloo.ca/jmai/GRIP-GL/landcover_NALCMS_GreatLakes.txt.zip (ASCII raster format; 345.5 MB)<br>
-http://www.civil.uwaterloo.ca/jmai/GRIP-GL/landcover_NALCMS_GreatLakes.tif.zip (TIF format incl projection information; 370.9 MB)
+http://www.civil.uwaterloo.ca/jmai/GRIP-GL/landcover_NALCMS_GreatLakes_v1.0.txt.zip (ASCII raster format; 345.5 MB)<br>
+http://www.civil.uwaterloo.ca/jmai/GRIP-GL/landcover_NALCMS_GreatLakes.tif_v1.1.zip (TIF format incl projection information; 817.5 MB)
 
 Aggregated landcover to RDRS-v2 and WFDEI-GEM-CaPA grid as external download link from: <br>
-http://www.civil.uwaterloo.ca/jmai/GRIP-GL/landcover_NALCMS_GreatLakes_aggregated.zip (ASCII raster format; 1.3MB)
+http://www.civil.uwaterloo.ca/jmai/GRIP-GL/landcover_NALCMS_GreatLakes_aggregated_v1.1.zip (ASCII raster format; 1.3MB)
 
 1.a Data (ASCII raster format; classification information given in separate file)
 
--- scripts/convert_aggregated2netcdf/convert.sh
+++ scripts/convert_aggregated2netcdf/convert.sh
@@ -31,135 +31,138 @@ pid=$$
 
 datapath="../../data/"
 
-convert_soil_GSDE=1
-convert_landcover_NACLMS=0
+convert_soil_GSDE=0
+convert_landcover_NACLMS=1
 
 
-grid='RDRS-v2'
-grid='WFDEI-GEM-CaPA'
+grids='RDRS-v2 WFDEI-GEM-CaPA'
 
-# -------------------
-# convert soil classes of GSDE
-# -------------------
-if [[ ${convert_soil_GSDE} -eq 1 ]] ; then
-    echo "Convert aggregated soil classes and texture based on GSDE into ${grid} grid ..."
 
-    if [[ ${grid} == 'RDRS-v2' ]] ; then
-	inputfile="${datapath}soilclass_GSDE_GreatLakes/soilclass_GSDE_GreatLakes_aggregated_v1.1/GL_GSDE_usda_soil_class_rdrs_v2.txt"
-	gridfile="${datapath}meteo_forcing_RDRS-v2/grip-gl_rdrs-v2-gridonly.nc"
-	legendsfile=""
-	varinfofile="${datapath}soilclass_GSDE_GreatLakes/soilclass_GSDE_GreatLakes_aggregated_v1.1/variable_info.csv"
-    else
-	if [[ ${grid} == 'WFDEI-GEM-CaPA' ]] ; then
-	    inputfile="${datapath}soilclass_GSDE_GreatLakes/soilclass_GSDE_GreatLakes_aggregated_v1.1/GL_GSDE_usda_soil_class_wfdei_gem_capa.txt"
-	    gridfile="${datapath}meteo_forcings_WFDEI-GEM-CaPA/grip-gl_wfdei-gem-capa_gridonly.nc"
+for grid in ${grids} ; do
+    
+    # -------------------
+    # convert soil classes of GSDE
+    # -------------------
+    if [[ ${convert_soil_GSDE} -eq 1 ]] ; then
+	echo "Convert aggregated soil classes and texture based on GSDE into ${grid} grid ..."
+
+	if [[ ${grid} == 'RDRS-v2' ]] ; then
+	    inputfile="${datapath}soilclass_GSDE_GreatLakes/soilclass_GSDE_GreatLakes_aggregated_v1.1/GL_GSDE_usda_soil_class_rdrs_v2.txt"
+	    gridfile="${datapath}meteo_forcing_RDRS-v2/grip-gl_rdrs-v2-gridonly.nc"
 	    legendsfile=""
 	    varinfofile="${datapath}soilclass_GSDE_GreatLakes/soilclass_GSDE_GreatLakes_aggregated_v1.1/variable_info.csv"
+	else
+	    if [[ ${grid} == 'WFDEI-GEM-CaPA' ]] ; then
+		inputfile="${datapath}soilclass_GSDE_GreatLakes/soilclass_GSDE_GreatLakes_aggregated_v1.1/GL_GSDE_usda_soil_class_wfdei_gem_capa.txt"
+		gridfile="${datapath}meteo_forcings_WFDEI-GEM-CaPA/grip-gl_wfdei-gem-capa_gridonly.nc"
+		legendsfile=""
+		varinfofile="${datapath}soilclass_GSDE_GreatLakes/soilclass_GSDE_GreatLakes_aggregated_v1.1/variable_info.csv"
+	    fi
 	fi
-    fi
 
-    if [ ! -e ${inputfile} ]; then
-	echo "Inputfile ${inputfile} does not exist!"
-	exit
-    fi
-
-    if [ ! -e ${gridfile} ]; then
-	echo "Grid defining file ${gridfile} does not exist!"
-	exit
-    fi
-
-    if [[ ${legendsfile} != "" ]] ; then
-	if [ ! -e ${legendsfile} ]; then
-	    echo "File with legends ${legendsfile} does not exist!"
+	if [ ! -e ${inputfile} ]; then
+	    echo "Inputfile ${inputfile} does not exist!"
 	    exit
 	fi
-    fi
 
-    if [[ ${varinfofile} != "" ]] ; then
-	if [ ! -e ${varinfofile} ]; then
-	    echo "File with variable infos ${varinfofile} does not exist!"
+	if [ ! -e ${gridfile} ]; then
+	    echo "Grid defining file ${gridfile} does not exist!"
 	    exit
 	fi
+
+	if [[ ${legendsfile} != "" ]] ; then
+	    if [ ! -e ${legendsfile} ]; then
+		echo "File with legends ${legendsfile} does not exist!"
+		exit
+	    fi
+	fi
+
+	if [[ ${varinfofile} != "" ]] ; then
+	    if [ ! -e ${varinfofile} ]; then
+		echo "File with variable infos ${varinfofile} does not exist!"
+		exit
+	    fi
+	fi
+
+	outputfile=$( echo $( echo ${inputfile} | rev | cut -d '/' -f 2- | rev )"_${grid}.nc" )
+
+	echo "python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -l ${varinfofile}"
+	python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -l ${varinfofile}
+
     fi
 
-    outputfile=$( echo $( echo ${inputfile} | rev | cut -d '/' -f 2- | rev )"_${grid}.nc" )
+    # -------------------
+    # convert aggreagted land cover classes of NACLMS
+    # -------------------
+    if [[ ${convert_landcover_NACLMS} -eq 1 ]] ; then
+	echo "Convert land cover NACLMS into ${grid} grid ..."
+	varname="perc_land_cover"
+	vartype="float"
+	unit="percent"
+	description="percentage of land cover type of NALCMS product for each forcing grid cell"
 
-    echo "python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -l ${varinfofile}"
-    python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -l ${varinfofile}
-
-fi
-
-# -------------------
-# convert aggreagted land cover classes of NACLMS
-# -------------------
-if [[ ${convert_landcover_NACLMS} -eq 1 ]] ; then
-    echo "Convert land cover NACLMS into ${grid} grid ..."
-    varname="perc_land_cover"
-    vartype="float"
-    unit="percent"
-    description="percentage of land cover type of NALCMS product for each forcing grid cell"
-
-    if [[ ${grid} == 'RDRS-v2' ]] ; then
-	inputfile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated/GL_NALCMS_landcover_rdrs_v2.txt"
-	gridfile="${datapath}meteo_forcing_RDRS-v2/grip-gl_rdrs-v2-gridonly.nc"
-	#
-	# # stores variables 4D (time, lat, lon, n_LC_types)
-	# legendsfile="${datapath}landcover_NALCMS_GreatLakes/NACLMS_legends.csv"
-	# varinfofile=""
-	#
-	# stores variables 3D (time, lat, lon) in <n_LC_types> separate variables
-	legendsfile=""  
-	varinfofile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated/variable_info.csv"
-    else
-	if [[ ${grid} == 'WFDEI-GEM-CaPA' ]] ; then
-	    inputfile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated/GL_NALCMS_landcover_wfdei_gem_capa.txt"
-	    gridfile="${datapath}meteo_forcings_WFDEI-GEM-CaPA/grip-gl_wfdei-gem-capa_gridonly.nc"
+	if [[ ${grid} == 'RDRS-v2' ]] ; then
+	    inputfile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated_v1.1/GL_NALCMS_landcover_rdrs_v2.txt"
+	    gridfile="${datapath}meteo_forcing_RDRS-v2/grip-gl_rdrs-v2-gridonly.nc"
 	    #
 	    # # stores variables 4D (time, lat, lon, n_LC_types)
 	    # legendsfile="${datapath}landcover_NALCMS_GreatLakes/NACLMS_legends.csv"
 	    # varinfofile=""
 	    #
 	    # stores variables 3D (time, lat, lon) in <n_LC_types> separate variables
-	    legendsfile="" 
-	    varinfofile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated/variable_info.csv"
+	    legendsfile=""  
+	    varinfofile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated_v1.1/variable_info.csv"
+	else
+	    if [[ ${grid} == 'WFDEI-GEM-CaPA' ]] ; then
+		inputfile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated_v1.1/GL_NALCMS_landcover_wfdei_gem_capa.txt"
+		gridfile="${datapath}meteo_forcings_WFDEI-GEM-CaPA/grip-gl_wfdei-gem-capa_gridonly.nc"
+		#
+		# # stores variables 4D (time, lat, lon, n_LC_types)
+		# legendsfile="${datapath}landcover_NALCMS_GreatLakes/NACLMS_legends.csv"
+		# varinfofile=""
+		#
+		# stores variables 3D (time, lat, lon) in <n_LC_types> separate variables
+		legendsfile="" 
+		varinfofile="${datapath}landcover_NALCMS_GreatLakes/landcover_NALCMS_GreatLakes_aggregated_v1.1/variable_info.csv"
+	    fi
 	fi
-    fi
 
-    if [ ! -e ${inputfile} ]; then
-	echo "Inputfile ${inputfile} does not exist!"
-	exit
-    fi
-
-    if [ ! -e ${gridfile} ]; then
-	echo "Grid defining file ${gridfile} does not exist!"
-	exit
-    fi
-
-    if [[ ${legendsfile} != "" ]] ; then
-	if [ ! -e ${legendsfile} ]; then
-	    echo "File with legends ${legendsfile} does not exist!"
+	if [ ! -e ${inputfile} ]; then
+	    echo "Inputfile ${inputfile} does not exist!"
 	    exit
 	fi
-    fi
 
-    if [[ ${varinfofile} != "" ]] ; then
-	if [ ! -e ${varinfofile} ]; then
-	    echo "File with variable infos ${varinfofile} does not exist!"
+	if [ ! -e ${gridfile} ]; then
+	    echo "Grid defining file ${gridfile} does not exist!"
 	    exit
 	fi
+
+	if [[ ${legendsfile} != "" ]] ; then
+	    if [ ! -e ${legendsfile} ]; then
+		echo "File with legends ${legendsfile} does not exist!"
+		exit
+	    fi
+	fi
+
+	if [[ ${varinfofile} != "" ]] ; then
+	    if [ ! -e ${varinfofile} ]; then
+		echo "File with variable infos ${varinfofile} does not exist!"
+		exit
+	    fi
+	fi
+
+	outputfile=$( echo $( echo ${inputfile} | rev | cut -d '/' -f 2- | rev )"_${grid}.nc" )
+
+	# stores variables 4D (time, lat, lon, n_LC_types)
+	# python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -v "${varname},${vartype},${unit},${description}" -a ${legendsfile}
+
+	# stores variables 3D (time, lat, lon) in <n_LC_types> separate variables
+	echo "python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -v "${varname},${vartype},${unit},${description}" -l ${varinfofile}"
+	python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -v "${varname},${vartype},${unit},${description}" -l ${varinfofile}
+	
     fi
 
-    outputfile=$( echo $( echo ${inputfile} | rev | cut -d '/' -f 2- | rev )"_${grid}.nc" )
-
-    # stores variables 4D (time, lat, lon, n_LC_types)
-    # python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -v "${varname},${vartype},${unit},${description}" -a ${legendsfile}
-
-    # stores variables 3D (time, lat, lon) in <n_LC_types> separate variables
-    echo "python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -v "${varname},${vartype},${unit},${description}" -l ${varinfofile}"
-    python aggregated2netcdf.py -i ${inputfile} -g ${gridfile} -o ${outputfile} -v "${varname},${vartype},${unit},${description}" -l ${varinfofile}
-    
-
-fi
+done # loop over grids
 
 exit 0
 
